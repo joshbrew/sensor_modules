a45be9c795939e614191cddc3bfb4d0c48b83ee2
Bubba Skrimp <brewster.joshua1@gmail.com>
2022-10-10 21:19:23 -0700

    latest, not sure why node_modules is pushing

--- index.ts
+++ index.ts
@@ -1,7 +1,101 @@
 import { initDevice } from "device-decoder";
+import { CanvasControls } from "graphscript/dist/services/worker/WorkerCanvas.js";
 
 import * as components from './components/index.js';
 
+
+import newPlot from "./modules/webglplot/plotter.js";
+
+let canvas = document.createElement('canvas');
+let overlay = document.createElement('canvas');
+
+canvas.style.backgroundColor = 'black';
+canvas.style.position = 'absolute';
+canvas.style.width = '300px';
+canvas.style.height = '300px';
+canvas.width = 500;
+canvas.height = 500;
+overlay.style.position = 'absolute';
+overlay.style.width = canvas.style.width;
+overlay.style.height = canvas.style.height;
+overlay.width = 500;
+overlay.height = 500;
+document.body.appendChild(canvas);
+document.body.appendChild(overlay);
+
+let sampleCt = 1000
+
+let plot = newPlot({
+    worker:true, //use an offscreen canvas
+    canvas,
+    overlay,
+    overlayFont:'1em Verdana',
+    overlayColor:'orange',
+    lines:{
+        '0':{
+            values:new Array(sampleCt).fill(0).map((v,i)=>{ return Math.sin(2*Math.PI*(5)*(Date.now()/1000+(i/sampleCt))); }),
+            width:0.1,
+            color:[0,255,255,1],
+            ymin:-0.5
+        },
+        '1':{
+            values:new Array(sampleCt).fill(0).map((v,i)=>{ return 1+Math.sin(2*Math.PI*(15)*(Date.now()/1000+(i/sampleCt))); }),
+            //width:2,
+            color:[0,255,0,1]
+        },
+        '2':{
+            values:new Array(sampleCt).fill(0).map((v,i)=>{ return 2+0.5*Math.sin(2*Math.PI*(10)*(Date.now()/1000+(i/sampleCt))); }),
+            //width:2,
+            color:[255,255,0,1]
+        },
+        '3':{
+            values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(25)*(Date.now()/1000+(i/sampleCt))); }),
+            //width:2,
+            color:[0,255,0,1]
+        },
+        '4':{
+            values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(1)*(Date.now()/1000+(i/sampleCt))); }),
+            //width:2,
+            color:[0,255,0,1]
+        },
+        '5':{
+            values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(3)*(Date.now()/1000+(i/sampleCt))); }),
+            //width:2,
+            color:[0,255,0,1]
+        }
+    }
+}) as CanvasControls; //returns result of Renderer call from workerCanvasRoutes
+
+console.log("Plot: ", plot);
+
+let anim = () => {
+    //setTimeout(() => {
+        plot.update({
+            '0':{
+                values:new Array(sampleCt).fill(0).map((v,i)=>{ return Math.sin(2*Math.PI*(5)*(Date.now()/1000+(i/sampleCt))); }),
+            },
+            '1':{
+                values:new Array(sampleCt).fill(0).map((v,i)=>{ return 1+Math.sin(2*Math.PI*(15)*(Date.now()/1000+(i/sampleCt))); }),
+            },
+            '2':{
+                values:new Array(sampleCt).fill(0).map((v,i)=>{ return 2+0.5*Math.sin(2*Math.PI*(10)*(Date.now()/1000+(i/sampleCt))); }),
+            },
+            '3':{
+                values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(25)*(Date.now()/1000+(i/sampleCt))); }),
+            },
+            '4':{
+                values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(1)*(Date.now()/1000+(i/sampleCt))); }),
+            },
+            '5':{
+                values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(3)*(Date.now()/1000+(i/sampleCt))); }),
+            }
+        });
+        
+        requestAnimationFrame(anim);
+    //}, 100);
+}
+
+anim();
 
 let connect = document.createElement('button');
 connect.innerText = 'Connect'
@@ -15,10 +109,9 @@ connect.onclick = () => {
                 '0002cafe-b0ba-8bad-f00d-deadbeef0000':(data:{
                     [key:string]:number[]
                 })=>{
-                    console.log(data)
-                    components.eeg.default(data)
-                    components.emg.default(data)
-
+                    console.log(data);
+                    components.eeg.default(data);
+                    components.emg.default(data);
                 }, //ads131m08 (main)
                 '0003cafe-b0ba-8bad-f00d-deadbeef0000':(data:{
                     red:number[],
@@ -26,10 +119,8 @@ connect.onclick = () => {
                     max_dietemp:number,
                     timestamp:number
                 })=>{
-
-                    console.log(data)
-                    components.heg.default(data)
-
+                    console.log(data);
+                    components.heg.default(data);
                 }, //max30102
                 '0004cafe-b0ba-8bad-f00d-deadbeef0000':(data:{
                     ax:number[],
@@ -40,26 +131,24 @@ connect.onclick = () => {
                     mpu_dietemp:number,
                     timestamp:number
                 })=>{
-                    console.log(data)
+                    console.log(data);
                     components.generic.accelerometer({
                         x:data.ax,
                         y:data.ay,
                         z:data.az,
                         timestamp: data.timestamp
-                    })
-
+                    });
                     components.generic.gyroscope({
                         x:data.gx,
                         y:data.gy,
                         timestamp: data.timestamp
-                    })
-
+                    });
                 }, //mpu6050
                 '0005cafe-b0ba-8bad-f00d-deadbeef0000':(data:{
                     [key:string]:number[] })=>{
-                    console.log(data)
-                    components.eeg.default(data)
-                    components.emg.default(data)
+                    console.log(data);
+                    components.eeg.default(data);
+                    components.emg.default(data);
 
                 }, //extra ads131 (if plugged in)
                 '0006cafe-b0ba-8bad-f00d-deadbeef0000':(data:{
@@ -69,11 +158,11 @@ connect.onclick = () => {
                     altitude:number[]
                 })=>{
 
-                    console.log(data)
-                    components.environment.temperature(data)
-                    components.environment.pressure(data)
-                    components.environment.humidity(data)
-                    components.environment.altitude(data)
+                    console.log(data);
+                    components.environment.temperature(data);
+                    components.environment.pressure(data);
+                    components.environment.humidity(data);
+                    components.environment.altitude(data);
 
                 } //bme280
             }
---
+++ modules/webglplot/plotter.ts
@@ -0,0 +1,52 @@
+import {WebglLinePlotUtil, WebglLinePlotProps} from 'webgl-plot-utils';
+let plotter = new WebglLinePlotUtil();
+
+import canvasworker from './canvas.worker'
+
+import { CanvasControls, CanvasProps, workerCanvasRoutes } from 'graphscript/services/worker/WorkerCanvas' //../../../GraphServiceRouter/services/worker/WorkerCanvas'//'graphscript/services/worker/WorkerCanvas';
+
+
+export default function newPlot(
+    options:CanvasProps & {
+        overlay?:HTMLCanvasElement, 
+        worker?:boolean|Worker|string|Blob|MessagePort, 
+        route?:string
+    } & WebglLinePlotProps
+) {
+
+    //provide the functions for the canvas routes, in our case wrapping the webglplot renderer instead of our own canvas render
+    let init = (options, canvas, context) => {
+        plotter.initPlot(options);
+    }
+
+    let update = (options, canvas, context, input) => {
+        //console.log('update plotter')
+        plotter.update(options._id, input);
+    }
+
+    let clear = (options, canvas, context) => {
+        plotter.deinitPlot(options._id);   
+    }
+
+    options.init = init;
+    options.update = update;
+    options.clear = clear;
+
+    if(options.worker) {
+
+        if(options.worker === true) {
+            options.worker = new Worker(canvasworker);
+        }
+        else if (typeof options.worker === 'string' || options.worker instanceof Blob) {
+            options.worker = new Worker(options.worker as any);
+        }
+
+        if(options.overlay) {
+            options.overlay = (options.overlay as any).transferControlToOffscreen();
+            options.transfer = [options.overlay];
+        }
+    }
+
+    return workerCanvasRoutes.Renderer(options as any) as CanvasControls;
+
+}
