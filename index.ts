4a98fb0e103954fc145b1976f3dd7339bd76e9f7
Garrett <gflynn@usc.edu>
2022-10-11 11:25:40 -0700

    Modularized for WASL

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import { CanvasControls } from "graphscript/dist/services/worker/WorkerCanvas.js
 import * as components from './components/index.js';
 
 
-import newPlot from "./modules/webglplot/plotter.js";
+import * as plotter from "./modules/webglplot/plotter.js";
 
 let canvas = document.createElement('canvas');
 let overlay = document.createElement('canvas');
@@ -25,72 +25,75 @@ document.body.appendChild(overlay);
 
 let sampleCt = 1000
 
-let plot = newPlot({
-    worker:true, //use an offscreen canvas
-    canvas,
-    overlay,
-    overlayFont:'1em Verdana',
-    overlayColor:'orange',
-    lines:{
-        '0':{
-            values:new Array(sampleCt).fill(0).map((v,i)=>{ return Math.sin(2*Math.PI*(5)*(Date.now()/1000+(i/sampleCt))); }),
-            width:0.1,
-            color:[0,255,255,1],
-            ymin:-0.5
-        },
-        '1':{
-            values:new Array(sampleCt).fill(0).map((v,i)=>{ return 1+Math.sin(2*Math.PI*(15)*(Date.now()/1000+(i/sampleCt))); }),
-            //width:2,
-            color:[0,255,0,1]
-        },
-        '2':{
-            values:new Array(sampleCt).fill(0).map((v,i)=>{ return 2+0.5*Math.sin(2*Math.PI*(10)*(Date.now()/1000+(i/sampleCt))); }),
-            //width:2,
-            color:[255,255,0,1]
-        },
-        '3':{
-            values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(25)*(Date.now()/1000+(i/sampleCt))); }),
-            //width:2,
-            color:[0,255,0,1]
-        },
-        '4':{
-            values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(1)*(Date.now()/1000+(i/sampleCt))); }),
-            //width:2,
-            color:[0,255,0,1]
-        },
-        '5':{
-            values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(3)*(Date.now()/1000+(i/sampleCt))); }),
-            //width:2,
-            color:[0,255,0,1]
-        }
-    }
-}) as CanvasControls; //returns result of Renderer call from workerCanvasRoutes
 
-console.log("Plot: ", plot);
-
-let anim = () => {
-    //setTimeout(() => {
-        plot.update({
+const plotterInstance = Object.assign(Object.assign({}, plotter), {
+    options: {
+        worker:true, //use an offscreen canvas
+        canvas,
+        overlay,
+        overlayFont:'1em Verdana',
+        overlayColor:'orange',
+        lines:{
             '0':{
                 values:new Array(sampleCt).fill(0).map((v,i)=>{ return Math.sin(2*Math.PI*(5)*(Date.now()/1000+(i/sampleCt))); }),
+                width:0.1,
+                color:[0,255,255,1],
+                ymin:-0.5
             },
             '1':{
                 values:new Array(sampleCt).fill(0).map((v,i)=>{ return 1+Math.sin(2*Math.PI*(15)*(Date.now()/1000+(i/sampleCt))); }),
+                //width:2,
+                color:[0,255,0,1]
             },
             '2':{
                 values:new Array(sampleCt).fill(0).map((v,i)=>{ return 2+0.5*Math.sin(2*Math.PI*(10)*(Date.now()/1000+(i/sampleCt))); }),
+                //width:2,
+                color:[255,255,0,1]
             },
             '3':{
                 values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(25)*(Date.now()/1000+(i/sampleCt))); }),
+                //width:2,
+                color:[0,255,0,1]
             },
             '4':{
                 values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(1)*(Date.now()/1000+(i/sampleCt))); }),
+                //width:2,
+                color:[0,255,0,1]
             },
             '5':{
                 values:new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(3)*(Date.now()/1000+(i/sampleCt))); }),
+                //width:2,
+                color:[0,255,0,1]
             }
-        });
-        
+        }
+    }
+});
+
+let plot = plotterInstance.create()
+console.log("Plot: ", plot);
+
+let anim = () => {
+
+    const arr1 = new Array(sampleCt).fill(0).map((v,i)=>{ return Math.sin(2*Math.PI*(5)*(Date.now()/1000+(i/sampleCt))); })
+    const arr2 = new Array(sampleCt).fill(0).map((v,i)=>{ return 1+Math.sin(2*Math.PI*(15)*(Date.now()/1000+(i/sampleCt))); })
+    const arr3 = new Array(sampleCt).fill(0).map((v,i)=>{ return 2+0.5*Math.sin(2*Math.PI*(10)*(Date.now()/1000+(i/sampleCt))); })
+    const arr4 = new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(25)*(Date.now()/1000+(i/sampleCt))); })
+    const arr5 = new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(1)*(Date.now()/1000+(i/sampleCt))); })
+    const arr6 = new Array(sampleCt).fill(0).map((v,i)=>{ return 0.5*Math.sin(2*Math.PI*(3)*(Date.now()/1000+(i/sampleCt))); })
+
+    plotterInstance.default([arr1, arr2, arr3, arr4, arr5, arr6])
+    // plotterInstance.default({
+    //     '0':{
+    //         values:arr1,
+    //     },
+    //     '1':arr2,
+    //     '2':arr3,
+    //     '3': arr4,
+    //     '4': arr5,
+    //     '5': arr6
+    // })
+
+    //setTimeout(() => {
         requestAnimationFrame(anim);
     //}, 100);
 }
--- modules/webglplot/plotter.ts
+++ modules/webglplot/plotter.ts
@@ -5,51 +5,52 @@ import canvasworker from './canvas.worker'
 
 import { CanvasControls, CanvasProps, workerCanvasRoutes } from 'graphscript/services/worker/WorkerCanvas' //../../../GraphServiceRouter/services/worker/WorkerCanvas'//'graphscript/services/worker/WorkerCanvas';
 
+// provide the functions for the canvas routes, in our case wrapping the webglplot renderer instead of our own canvas render
+const init = (options, canvas, context) => {
+    plotter.initPlot(options);
+}
 
-export default function newPlot(
-    options:CanvasProps & {
+const update = (options, canvas, context, input) => {
+    //console.log('update plotter')
+    plotter.update(options._id, input);
+}
+
+const clear = (options, canvas, context) => {
+    plotter.deinitPlot(options._id);   
+}
+
+export let plot:CanvasControls
+
+export let options: CanvasProps & {
         overlay?:HTMLCanvasElement, 
         worker?:boolean|Worker|string|Blob|MessagePort, 
         route?:string
     } & WebglLinePlotProps
-) {
 
-    //provide the functions for the canvas routes, in our case wrapping the webglplot renderer instead of our own canvas render
-    let init = (options, canvas, context) => {
-        plotter.initPlot(options);
-    }
 
-    let update = (options, canvas, context, input) => {
-        //console.log('update plotter')
-        plotter.update(options._id, input);
-    }
+export function create() {
 
-    let clear = (options, canvas, context) => {
-        plotter.deinitPlot(options._id);   
-    }
-
-    //let draw = (options, canvas, context) => {}
-
+    const options = this.options
     options.init = init;
     options.update = update;
     options.clear = clear;
-    //options.draw = draw;
 
+    console.log('options', options)
     if(options.worker) {
 
-        if(options.worker === true) {
-            options.worker = new Worker(canvasworker);
-        }
-        else if (typeof options.worker === 'string' || options.worker instanceof Blob) {
-            options.worker = new Worker(options.worker as any);
-        }
-
+        if(options.worker === true) options.worker = new Worker(canvasworker);
+        else if (typeof options.worker === 'string' || options.worker instanceof Blob) options.worker = new Worker(options.worker as any);
+        
         if(options.overlay) {
             options.overlay = (options.overlay as any).transferControlToOffscreen();
             options.transfer = [options.overlay];
         }
     }
 
-    return workerCanvasRoutes.Renderer(options as any) as CanvasControls;
+    this.plot = workerCanvasRoutes.Renderer(options) as CanvasControls;
+    return this.plot
+}
 
+export default function (...args) {
+   if (this.plot) this.plot.update(...args);
 }
